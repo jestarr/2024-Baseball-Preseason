9646581a1588270c3d79c50699948a01e0660244
Jonathan Starr <jonathanestarr@gmail.com>
2024-03-19 13:03:44 -0400

    Update for March 15 Ranks

--- data-raw/get_eno-sp-ranks.R
+++ data-raw/get_eno-sp-ranks.R
@@ -3,7 +3,7 @@ library(starrbaseball)
 # first get sheet names to know what to use
 get_eno_sp_rankings(sheet_names = TRUE)
 
-eno <- get_eno_sp_rankings(sheet = "ranks mar4",sheet_names = FALSE)
+eno <- get_eno_sp_rankings(sheet = "WorkingRanksMar15", sheet_names = FALSE)
 
 
 readr::write_excel_csv(eno, "data-raw/2024_eno-sarris-pitching-ranks.csv")
